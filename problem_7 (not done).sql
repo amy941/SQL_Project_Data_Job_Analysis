696172bcf85293dea6b4eb818389c61641236dde
Amy Minh Pham <100558060+amy941@users.noreply.github.com>
2024-11-06 15:08:30 -0700

    advanced_sql

---
+++ problem_7 (not done).sql
@@ -0,0 +1,12 @@
+/*
+Find the count of the number of remote job postings per skill
+    - Display the top 5 skills by their demand in remote jobs
+    - Include skill ID, name, and count of postings requiring the skill
+*/
+
+SELECT
+    job_postings.job_id,
+    skill_id 
+FROM
+    skills_job_dim AS skills_to_job
+INNER JOIN job_postings_fact AS job_postings ON job_postings.job_id = skills_to_job.job_id;
