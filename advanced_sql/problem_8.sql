83d2697b8f75d472910c3f6567cc7ee98102e4a2
Amy Minh Pham <100558060+amy941@users.noreply.github.com>
2024-11-30 19:15:32 -0700

    push querries

--- advanced_sql/problem_8.sql
+++ advanced_sql/problem_8.sql
@@ -1,21 +1,27 @@
+/* 
+Find job postings from the first quarter that have a salary greater than $70K.
+- combine job posting tables from the first quarter of 2023 (Jan-Mar)
+- get job postings with an avg yearly salary > $70K from the first quarter of 2023 (Jan-Mar)
+*/
+
 SELECT 
-    job_title_short,
-    job_location,
-    job_via,
-    job_posted_date::date,
-    salary_year_avg
+    quarter1_job_postings.job_title_short,
+    quarter1_job_postings.job_location,
+    quarter1_job_postings.job_via,
+    quarter1_job_postings.job_posted_date::DATE,
+    quarter1_job_postings.salary_year_avg
 FROM (
     SELECT *
     FROM january_jobs
-    UNION ALL 
+    UNION ALL
     SELECT *
     FROM february_jobs
     UNION ALL
     SELECT *
     FROM march_jobs
-) AS quarter1_job_postings
-WHERE 
-    salary_year_avg > 70000 AND
-    job_title_short = 'Data Analyst'
-ORDER BY
-    salary_year_avg DESC
+) AS quarter1_job_postings   
+WHERE   
+    quarter1_job_postings.salary_year_avg > 70000
+    AND quarter1_job_postings.job_title_short = 'Data Analyst'
+ORDER BY    
+    quarter1_job_postings.salary_year_avg DESC    
--- project_sql_1/1_top_paying_jobs.sql
+++ project_sql_1/1_top_paying_jobs.sql
@@ -1,5 +1,5 @@
 /*
-Question: What are the top-paying data analyst jobs?**
+Question: What are the top-paying data analyst jobs?
 - Identify the top 10 highest-paying Data Analyst roles that are available remotely.
 - Focuses on job postings with specified salaries (remvoves NULLs).
 - Why? Aims to highlight the top-paying opportunities for Data Analysts, offering insights into employment options and location flexibility.
@@ -18,6 +18,7 @@ SELECT
 FROM
 	job_postings_fact
 LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
+-- add company name from company dim table 
 WHERE
 	job_title_short= 'Data Analyst'
 	AND salary_year_avg IS NOT NULL
